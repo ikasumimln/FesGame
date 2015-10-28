4c3ea808cea97a4d5316a1785c5dc7a0032d44ee
ikasumimln <ikasumi.meron@gmail.com>
2015-10-29 03:41:47 +0900

    大幅削除 このクラスいらないかも

--- src/TimeTask.java
+++ src/TimeTask.java
@@ -1,27 +1,14 @@
 import java.text.NumberFormat;
-import java.util.Timer;
 import java.util.TimerTask;
 
 //TimerTask内部クラス
-public class TimeTask extends TimerTask {
-	public static double i = 0;
-    public static String time;
+class TimeTask extends TimerTask {
     public void run() {
-    	i += 0.01;
+    	MainPanel.i += 0.01;
 
     	NumberFormat format = NumberFormat.getInstance();
    		format.setMaximumFractionDigits(2);
-   		time = (format.format(i));
-   		System.out.println(time);
+   		MainPanel.time = (format.format(MainPanel.i));
+   		System.out.println(MainPanel.time);
     }
-}
-
-class TimerSample{
-    public static void main(String[] args) {
-    		// タイマーインスタンス作成
-    		Timer timer = new Timer();
-    		// タイマー開始　0.01秒ごとにタイマータスクTimeTaskを繰り返し
-    		timer.schedule(new TimeTask(), 0, 10);
-    }
-
 }
