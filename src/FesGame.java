0c177a1a1e46e3d82012c5484d4ff55f84b66d0b
ikasumimln <ikasumi.meron@gmail.com>
2015-11-06 02:02:09 +0900

    Revert "BGMと背景の追加"
    This reverts commit 2b4ad66dd595146e72c8061967da690d23672fa8.

--- src/FesGame.java
+++ src/FesGame.java
@@ -1,15 +1,14 @@
 import java.awt.Container;
-
 import javax.swing.JFrame;
 
 public class FesGame extends JFrame {
-
+	
 	public FesGame() {
 		// タイトルを設定
 		setTitle("仮完成");
 		// サイズ変更禁止
 		setResizable(false);
-
+		
 		// パネルを作成してフレームに追加
 		MainPanel panel = new MainPanel();
 		Container contentPane = getContentPane();
@@ -17,7 +16,7 @@ public class FesGame extends JFrame {
 		// パネルサイズに合わせてフレームサイズを自動設定
 		pack();
 	}
-
+	
 	public static void main(String[] args) {
 		// フレームをセット
 		FesGame frame = new FesGame();
